6e47a35c4aa49f2901db15bb50d11eca0a12c2fd
aohirot <hiromichiaoki19930426@aol.com>
2020-10-01 13:08:51 +0900

    ピンチで拡大を更に拡大できるようにした。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,94 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var UIImageView: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+    
+    var imageIndex = 0
 
+    var images : [UIImage]! = [
+        UIImage(named: "Image1.jpg")!,
+        UIImage(named: "Image2.jpg")!,
+        UIImage(named: "Image3.jpg")!]
+    
+    @objc func changeImage() {
+           imageIndex += 1
+        if (imageIndex == images.count) {
+            imageIndex = 0
+           }
+           UIImageView.image = images[imageIndex]
+       }
+    
+    // スライドショーに使用するタイマーを宣言
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let imageView = images[0]
+
+        UIImageView.image = imageView
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           // segueから遷移先のResultViewControllerを取得する
+        let imageZoomViewController: ImageZoomViewController = segue.destination as! ImageZoomViewController
+        imageZoomViewController.imageScreen = images[imageIndex]
+    }
+    
+    @IBAction func tapAction(_ sender: Any) {
+        // セグエを使用して画面を遷移
+        performSegue(withIdentifier: "ImageZoomViewController", sender: nil)
+        if (timer != nil) {
+            // 停止時の処理を実装
+            // タイマーを停止する
+            timer.invalidate()
+            //タイマーを削除しておく(timer.invalidateだけだとtimerがnilにならないため)
+            timer = nil
+            nextButton.isEnabled = true
+            backButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func backImage(_ sender: UIButton) {
+        if imageIndex == 0 {
+            imageIndex = 2
+        } else {
+            imageIndex -= 1
+        }
+        UIImageView.image = images[imageIndex]
+    }
+    
+    @IBAction func nextImage(_ sender: UIButton) {
+        if imageIndex == 2 {
+            imageIndex = 0
+        } else {
+            imageIndex += 1
+        }
+        UIImageView.image = images[imageIndex]
+    }
+    
+    @IBAction func startStop(_ sender: Any) {
+        
+        if (timer == nil) {
+        // 再生時の処理を実装
 
-
+        // タイマーをセットする
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+            nextButton.isEnabled = false
+            backButton.isEnabled = false
+            } else {
+            // 停止時の処理を実装
+            // タイマーを停止する
+            timer.invalidate()
+            //タイマーを削除しておく(timer.invalidateだけだとtimerがnilにならないため)
+            timer = nil
+            nextButton.isEnabled = true
+            backButton.isEnabled = true
+        }
 }
-
+}
